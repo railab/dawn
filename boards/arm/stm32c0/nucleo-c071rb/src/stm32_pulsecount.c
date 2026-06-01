b455e519e0876c85ac424ac9944102c766bdf1ed
raiden00pl <raiden00@railab.me>
2026-06-01 14:03:48 +0200

    migrate to new stm32 ports organisation in nuttx
    Signed-off-by: raiden00pl <raiden00@railab.me>

---
+++ boards/arm/stm32c0/nucleo-c071rb/src/stm32_pulsecount.c
@@ -0,0 +1,51 @@
+/*
+ * boards/arm/stm32c0/nucleo-c071rb/src/stm32_pulsecount.c
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#include <nuttx/config.h>
+
+#include <errno.h>
+
+#include <nuttx/timers/pulsecount.h>
+
+#include <arch/board/board.h>
+#include <stm32_pulsecount.h>
+
+#include "nucleo-c071rb.h"
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: stm32_pulsecount_setup
+ *
+ * Description:
+ *   Initialize pulsecount and register the pulsecount device.
+ *
+ ****************************************************************************/
+
+int stm32_pulsecount_setup(void)
+{
+  struct pulsecount_lowerhalf_s *pulsecount;
+  int ret;
+
+#ifdef CONFIG_STM32_TIM1_PULSECOUNT
+  pulsecount = stm32_pulsecountinitialize(1);
+  if (pulsecount == NULL)
+    {
+      return -ENODEV;
+    }
+
+  ret = pulsecount_register("/dev/pulsecount0", pulsecount);
+  if (ret < 0)
+    {
+      return ret;
+    }
+#endif
+
+  return OK;
+}
